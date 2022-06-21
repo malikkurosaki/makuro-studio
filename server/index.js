f846c25031d43e95d11726d92bfa007674dcdefb
malikkurosaki <kurosakiblackangel@gmail.com>
2022-06-21 23:27:40 +0800

    ya

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
