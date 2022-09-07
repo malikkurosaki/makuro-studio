e20d215320c9e56fc23a6378a0e362916b1c4bcf
malikkurosaki <kurosakiblackangel@gmail.com>
2022-09-07 19:05:08 +0800

    update

--- xbin/git_push.js
+++ xbin/git_push.js
@@ -2,6 +2,6 @@ const exec = require('child_process').execSync;
 const fs = require('fs');
 const path = require('path');
 
-exec(`git add . && git commit -m "update" && git push`);
+exec(`git add . && git commit -m "update" && git push`, {stdio: "inherit", cwd: path.join(__dirname, "./../")});
 const version = exec('git describe --tags --always --dirty').toString().trim();
 console.log(version);
