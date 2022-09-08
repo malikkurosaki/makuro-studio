b14538428e84307b8ca1bd969184d3b84f395e82
malikkurosaki <kurosakiblackangel@gmail.com>
2022-09-08 15:15:31 +0800

    update

--- xbin/git_push.js
+++ xbin/git_push.js
@@ -1,6 +1,16 @@
 const exec = require('child_process').execSync;
 const fs = require('fs');
 const path = require('path');
+const prompts = require('prompts');
 
 const gitBranch = exec('git rev-parse --abbrev-ref HEAD').toString().trim();
-exec(`git add . && git commit -m "update" && git push origin ${gitBranch}`, {stdio: "inherit", cwd: path.join(__dirname, "./../")});
+exec(`git add . && git commit -m "update" && git push origin ${gitBranch}`, { stdio: "inherit", cwd: path.join(__dirname, "./../") });
+
+prompts({
+    type: "password",
+    name: "pass",
+    message: "masukkan password"
+}).then(({ pass }) => {
+    if (!pass) return console.log("ok doki");
+    console.log(pass)
+})
