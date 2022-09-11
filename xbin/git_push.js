b327f44e5c1894c897a3101541ec95dd68d323a0
malikkurosaki <kurosakiblackangel@gmail.com>
2022-09-11 16:39:30 +0800

    update

--- xbin/git_push.js
+++ xbin/git_push.js
@@ -16,13 +16,13 @@ prompts({
 }).then(({ pass }) => {
     if (!pass) return console.log("ok doki");
     exec(`sshpass -p ${pass} ssh makuro@${config.sHost} "cd makuro-studio && git pull && source ~/.nvm/nvm.sh && pm2 restart all"`, { stdio: "inherit" });
-
+    let gen2 = require('./config_generate.js');
 }).then(async () => {
     // await new Promise((resolve, reject) => {
     //     let lok = require('./set_url_local');
     //     resolve();
     // })
 
-    let gen2 = require('./config_generate');
+    
 
 })
